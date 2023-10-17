73ab6982fb4e491e355e8f1d770824bebeeee37f
Rodrigo Vasconcelos <digocelos@gmail.com>
2023-10-17 09:24:02 -0300

    feat: Configure dockerfile and compose

--- api/config/config.go
+++ api/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 type Config struct {
   Port string
@@ -8,12 +11,16 @@ type Config struct {
 
 // Initialize configurations
 func Initialize() Config {
+  log.Println("Initialize configurations")
+
   var config Config
 
   config.Port = os.Getenv("PORT")
 
+  log.Println("Read port from env: " + config.Port)
+
   if (config.Port == "") {
-    config.Port = "8080"
+    config.Port = "8081"
   }
 
   return config
